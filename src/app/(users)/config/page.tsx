559459e4770ed7eba378e21080a9c463bc81e0d3
Enilson Neto <enilson.neto@thinkamconnect.com>
2024-09-23 17:03:10 -0300

    Links ativos

---
+++ src/app/(users)/config/page.tsx
@@ -0,0 +1,7 @@
+export default function Config () {
+    return (
+        <div>
+            <h1>Pagina Config</h1>
+        </div>
+    )
+}
---
+++ src/app/(users)/info/page.tsx
@@ -0,0 +1,7 @@
+export default function Info () {
+    return (
+        <div>
+            <h1>Pagina Info</h1>
+        </div>
+    )
+}
