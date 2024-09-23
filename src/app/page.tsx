c96f2cdbc8ae6482877e8450622c69bf3fc0a200
Enilson Neto <enilson.neto@thinkamconnect.com>
2024-09-23 16:38:29 -0300

    Variaveis para rotas importante para reaquisições de api

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,11 @@
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
+    <>
     <h1>Olá Next</h1>
+    <Link href={'/products'}>Ir para pagina de produtos</Link>
+    </>
   );
 }
